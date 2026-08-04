615d4c51a52f78e73aadf6052aa81727899915b7
qudgns8040 <qudgns8040@gmail.com>
2026-08-05 01:41:10 +0900

    join schedule data

--- modules/checklist/checklist_logic.py
+++ modules/checklist/checklist_logic.py
@@ -13,6 +13,33 @@ from .checklist_repository import (
 def get_today():
     return datetime.today().strftime("%Y-%m-%d")
 
+# 오늘 요일 반환
+def get_today_day():
+    day_map = {
+        0: "월",
+        1: "화",
+        2: "수",
+        3: "목",
+        4: "금",
+        5: "토",
+        6: "일",
+    }
+    return day_map[datetime.today().weekday()]
+def get_today_day_group():
+    #오늘 요일값
+    today_day = get_today_day()
+
+    day_group_map = {
+        "월": "월",
+        "수": "수금",
+        "금": "수금",
+        "화": "화목",
+        "목": "화목",
+        "토": "토",
+    }
+    return day_group_map.get(today_day)
+
+
 # 체크리스트_마스터 데이터프레임 생성
 def create_checklist_master():
 
@@ -53,24 +80,109 @@ def craete_schedule_master_target():
 
     return schedulemaster_target
 
+# 스케줄 생성 v1
+# 1. 스케줄마스터-스케줄마스터타겟 schedule_id로 1대다 조인
+def create_schedule_v1():
+    schedule = load_schedule_master()
+    target = load_schedule_master_target()
 
-###################################
-# 스케줄 생성하기_Proto
-###################################
-# 스케줄 생성 규칙
-# 조인 -> 오늘 조건 필터 -> 오늘 스케줄
+    merged_schedule = schedule.merge(
+        target,
+        on="schedule_id",
+        how="left",
+        validate="one_to_many"
+    )
 
-# def create_schedule():
+    return merged_schedule
+# 스케줄 생성 v2
+def create_schedule_v2():
+    schedule = create_schedule_v1()
 
-#     # check_id 기준 inner join
+    today_day_group = get_today_day_group()
 
+    #일요일
+    if today_day_group is None:
+        return schedule.iloc[0:0].copy()
 
+    # 여름방학 & 현재 요일과 매칭 & 활성화 상태 체크
+    today_schedule = schedule[
+        (schedule["schedule_group"] == "여름방학")
+        & (schedule["day_group"] == today_day_group)
+        & (schedule["schedule_is_active"] == "Y")
+    ].copy()
 
-#     master = load_checklist_master()
-#     rule = load_checklist_rule()
+    # 시간별 순서 정렬
+    today_schedule = today_schedule.sort_values(
+        by="start_time"
+    ).reset_index(drop=True)
 
-#     schedule = master.merge(
-#         rule,
-#         on="check_id"
-#         how="inner"
-#     )
+    return today_schedule
+
+# 스케줄 - 관련 마스터 데이터 전체 조인
+def create_schedule_dataset():
+    schedule = load_schedule_master()
+    target = load_schedule_master_target()
+    class_master = load_class_master()
+
+    # 일정 1 : 대상 반 N 조인
+    schedule_with_target = schedule.merge(
+        target,
+        on="schedule_id",
+        how="left",
+        validate="one_to_many"
+    )
+
+    # 대상 반 N : 학급 마스터 1 조인
+    full_schedule = schedule_with_target.merge(
+        class_master,
+        on="class_code",
+        how="left",
+        validate="many_to_one"
+    )
+    return full_schedule
+
+# 스케줄 - 오늘 일정에 맞게 필터링 로직
+def create_today_schedule():
+    schedule = create_schedule_dataset()
+    today_day_group = get_today_day_group()
+
+    if today_day_group is None:
+        return schedule.iloc[0:0].copy()
+
+    today_schedule = schedule[
+        (schedule["schedule_group"] == "여름방학")
+        &(schedule["day_group"] == today_day_group)
+        &(schedule["schedule_is_active"] == "Y")
+    ].copy()
+
+    return(
+        today_schedule
+        .sort_values("start_time")
+        .reset_index(drop=True)
+    )
+
+# 스케줄 - 실제 화면용 구조로 변경(그룹화)
+def create_schedule_view_data():
+    schedule = create_today_schedule()
+
+    grouped_schedule = (
+        schedule.groupby(
+            [
+                "schedule_id",
+                "schedule_type",
+                "start_time",
+                "end_time"
+            ],
+            dropna=False,
+            sort=False
+        )
+        .agg(
+            class_codes=(
+                "class_code",
+                lambda values: list(values.dropna())
+            )
+        )
+        .reset_index()
+    )
+
+    return grouped_schedule
--- modules/checklist/checklist_view.py
+++ modules/checklist/checklist_view.py
@@ -5,7 +5,12 @@ from .checklist_logic import (
     create_checklist_rule,
     create_class_master,
     create_schedule_master,
-    craete_schedule_master_target
+    craete_schedule_master_target,
+    create_schedule_v1,
+    create_schedule_v2,
+    create_schedule_dataset,
+    create_today_schedule,
+    create_schedule_view_data
 )
 
 # checklist_master 데이터프레임 출력 함수
@@ -52,6 +57,65 @@ def show_schedule_master_target():
     schedulemaster_target = craete_schedule_master_target()
     st.dataframe(schedulemaster_target)
 
+# 스케줄 v1 출력
+def show_schedule_v1():
+    st.title("스케줄 v1")
+    st.divider()
+
+    schedule = create_schedule_v1()
+    st.dataframe(schedule)
+
+def show_schedule_v2():
+    st.title("오늘 스케줄 v2")
+    st.divider()
+
+    schedule = create_schedule_v2()
+
+    if schedule.empty:
+        st.info("오늘은 등록된 스케줄이 없습니다."); return
+
+    st.dataframe(
+        schedule,
+        use_container_width=True,
+        hide_index=True,
+    )
+
+def show_schedule_dataset():
+    st.title("스케줄 데이터셋 2단 조인 출력 결과")
+    st.divider()
+
+    schedule = create_schedule_dataset()
+    st.dataframe(schedule)
+
+def show_today_schedule():
+    st.title("스케줄 데이터셋 요일 필터링 로직 결과")
+    st.divider()
+
+    today_schedule = create_today_schedule()
+    st.dataframe(today_schedule)
+
+def show_today_schedule2():
+    st.title("스케줄 데이터셋 그룹화 결과")
+    st.divider()
+
+    schedule = create_schedule_view_data()
+
+    if schedule.empty:
+        st.info("오늘 등록된 일정이 없습니다.")
+        return
+
+    for _, row in schedule.iterrows():
+        class_text = ",".join(row["class_codes"])
+
+        st.write(
+            f"**{row['start_time']} ~ {row['end_time']}**"
+        )
+
+        st.write(
+            f"**{row['schedule_type']} | {class_text}**"
+        )
+
+        st.divider()
 
 # 최종 출력 페이지
 def show_checklist_page():
@@ -61,6 +125,13 @@ def show_checklist_page():
     show_classmaster_section()
     show_schedule_master()
     show_schedule_master_target()
+    show_schedule_v1()
+    show_schedule_v2()
+    show_schedule_dataset()
+    show_today_schedule()
+    show_today_schedule2()
+
+    
 
 
     # # 구분별 출력
